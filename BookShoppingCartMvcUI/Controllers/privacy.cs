1b51f13dfb496eb49b81dd0262a11d2c3f9133a7
Hamza Ben bella <benbellah202@gmail.com>
2024-01-21 02:54:40 +0100

    Ajoutez des fichiers projet.

---
+++ BookShoppingCartMvcUI/Controllers/privacy.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookShoppingCartMvcUI.Controllers
+{
+    public class PrivacyController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Supposons que vous avez les détails du livre, tels que l'image et les informations, dans votre base de données ou ailleurs.
+            ViewBag.BookImage = "/images/your_book_image.jpg";
+            ViewBag.BookDetails = "Description du livre, auteur, etc.";
+
+            return View();
+        }
+    }
+}
